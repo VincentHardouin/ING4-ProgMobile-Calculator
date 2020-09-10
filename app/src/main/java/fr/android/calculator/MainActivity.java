862aedaa142b7528eade673f07581478c7bd43d6
Vincent Hardouin <vincent.hardouin@pix.fr>
2020-09-10 17:54:46 +0200

    Initial commit

---
+++ app/src/main/java/fr/android/calculator/MainActivity.java
@@ -0,0 +1,35 @@
+package fr.android.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void myClickHandler(View view) {
+        switch (view.getId()) {
+            case R.id.button0:
+            case R.id.button1:
+            case R.id.button2:
+            case R.id.button3:
+            case R.id.button4:
+            case R.id.button5:
+            case R.id.button6:
+            case R.id.button7:
+            case R.id.button8:
+            case R.id.button9:
+            case R.id.buttonAddition:
+            case R.id.buttonSoustraction:
+            case R.id.buttonMultiplication:
+            case R.id.buttonEqual:
+        }
+    }
+}
